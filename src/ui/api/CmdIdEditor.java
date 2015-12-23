30ac25e0d3f13d63095bfde1ff93f849fed05872
Abdelrahman Mohsen <abdelrahman.waziry@gmail.com>
2015-12-24 00:20:49 +0200

    Implement Argument Editors

---
+++ src/ui/api/CmdIdEditor.java
@@ -0,0 +1,51 @@
+package ui.api;
+
+import engine.Argument;
+import engine.Engine;
+import engine.Parameter;
+import engine.Type;
+import java.awt.Component;
+import javax.swing.*;
+
+public class CmdIdEditor extends JPanel implements ArgumentEditor {
+
+    private final JComboBox commandSelector;
+    private final JLabel label;
+    private Parameter param;
+    
+    public CmdIdEditor() {
+        commandSelector = new JComboBox(Engine.getInstance().getCommandsNames());
+        label = new JLabel();
+        add(label);
+        add(commandSelector);
+    }
+    
+    
+    @Override
+    public Component getEditorPanel() {
+        return this;
+    }
+
+    @Override
+    public Argument getArgument() {
+        Argument arg = new Argument(Type.CMD_ID, commandSelector.getSelectedItem());
+        return arg;
+    }
+
+    @Override
+    public void setParameter(Parameter param) {
+        this.param = param;
+        label.setText(param.NAME);
+    }
+
+    @Override
+    public Parameter getParameter() {
+        return param;
+    }
+
+    @Override
+    public boolean isArgumentValid() {
+        return true;
+    }
+    
+}
---
+++ src/ui/api/StringEditor.java
@@ -0,0 +1,51 @@
+package ui.api;
+
+import engine.Argument;
+import engine.Parameter;
+import engine.Type;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class StringEditor extends JPanel implements ArgumentEditor{
+    
+    private final JTextField argumentValue;
+    private final JLabel label;
+    private Parameter param;
+    
+    public StringEditor(){
+        argumentValue = new JTextField();
+        label = new JLabel();
+        add(label);
+        add(argumentValue);
+    }
+    
+    @Override
+    public Component getEditorPanel() {
+        return this;
+    }
+
+    @Override
+    public Argument getArgument() {
+        Argument arg = new Argument(Type.STR, argumentValue.getText());
+        return arg;
+    }
+
+    @Override
+    public void setParameter(Parameter param) {
+        this.param = param;
+        label.setText(param.NAME);
+    }
+
+    @Override
+    public Parameter getParameter() {
+        return param;
+    }
+
+    @Override
+    public boolean isArgumentValid() {
+        return true;
+    }
+    
+}

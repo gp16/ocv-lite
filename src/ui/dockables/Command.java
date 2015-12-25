5c57690613eb2085588a964cbd2d6bf6f0477a0f
Abdelrahman Mohsen <abdelrahman.waziry@gmail.com>
2015-12-25 21:23:46 +0200

    Change command layout

--- src/ui/dockables/Command.java
+++ src/ui/dockables/Command.java
@@ -4,22 +4,28 @@ import engine.Engine;
 import engine.ICommand;
 import engine.Parameter;
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import javax.swing.JComboBox;
-import javax.swing.JPanel;
+import javax.swing.*;
+import ui.api.ArgumentEditor;
+import ui.api.CmdIdEditor;
 import ui.api.Dockable;
+import ui.api.StringEditor;
 
 public class Command extends JPanel implements Dockable {
     
     private JComboBox commandSelector;
-    private JPanel panel = new JPanel(); 
+    private JPanel panel = new JPanel();
+    private JButton executeButton;
     
     public Command()
     {
-        add(panel,BorderLayout.WEST);
+        add(panel,CardLayout.class);
         commandSelector = new JComboBox(Engine.getInstance().getCommandsNames());
         commandSelector.addItemListener(new ItemListener() {
             @Override
@@ -29,13 +35,25 @@ public class Command extends JPanel implements Dockable {
                    ICommand comm = Engine.getInstance()
                            .getCommand(e.getItem().toString());
                    Parameter [] params = comm.getParams();
+                   for (int i=0; i < params.length; i++) {
+                    ArgumentEditor argumentEditor = ArgumentEditor.createInstance(params[i]);
+                 if(argumentEditor != null)
+                    panel.add(argumentEditor.getEditorPanel());
+                  }
                    panel.setLayout(new GridLayout(params.length, 2));
                    panel.revalidate();
                }
             }
         });
     }
-
+/*
+    public void execute() {
+        executeComm = new ();
+            executeButton.addActionListener((ActionEvent e) -> {
+        executeComm.execute();
+    });
+    }
+    */
     @Override
     public Component[] getNavigationComponents()
     {

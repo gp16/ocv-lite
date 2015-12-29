cdec88e7c1c86c2db3e20bd76227dcee391e55f5
Abdelrahman Mohsen <abdelrahman.waziry@gmail.com>
2015-12-29 20:21:45 +0200

    Render commands components

--- src/ui/dockables/Command.java
+++ src/ui/dockables/Command.java
@@ -11,43 +11,46 @@ import java.awt.event.ItemListener;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import ui.api.Dockable;
+import ui.argumenteditors.ArgumentEditor;
 
 public class Command extends JPanel implements Dockable {
-    
+
     private JComboBox commandSelector;
-    private JPanel panel = new JPanel(); 
-    
-    public Command()
-    {
-        add(panel,BorderLayout.WEST);
+    private JPanel panel = new JPanel();
+
+    public Command() {
+        add(panel, BorderLayout.WEST);
         commandSelector = new JComboBox(Engine.getInstance().getCommandsNames());
         commandSelector.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 panel.removeAll();
-               if (e.getStateChange() == ItemEvent.SELECTED ) {
-                   ICommand comm = Engine.getInstance()
-                           .getCommand(e.getItem().toString());
-                   Parameter [] params = comm.getParams();
-                   panel.setLayout(new GridLayout(params.length, 2));
-                   panel.revalidate();
-               }
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    ICommand comm = Engine.getInstance()
+                            .getCommand(e.getItem().toString());
+                    Parameter[] params = comm.getParams();
+                    for (int i = 0; i < params.length; i++) {
+                        ArgumentEditor argumentEditor = ArgumentEditor.createInstance(params[i]);
+                        if (argumentEditor != null) {
+                            panel.add(argumentEditor.getEditorPanel());
+                        }
+                    }
+                    panel.setLayout(new GridLayout(params.length, 2));
+                    panel.revalidate();
+                }
             }
         });
     }
 
     @Override
-    public Component[] getNavigationComponents()
-    {
-	return new Component[] {
-            commandSelector,
-	};
+    public Component[] getNavigationComponents() {
+        return new Component[]{
+            commandSelector,};
     }
 
     @Override
-    public JPanel getDockablePanel()
-    {
-	return this;
+    public JPanel getDockablePanel() {
+        return this;
     }
-    
+
 }

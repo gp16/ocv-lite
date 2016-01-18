56517eb4600c473682371f47d00173b9be7f1fd6
amrgamal <amr.gamal9441@gmail.com>
2016-01-18 07:15:02 +0200

    Image is now uneditable

--- src/ui/dockables/Image.java
+++ src/ui/dockables/Image.java
@@ -1,9 +1,15 @@
 package ui.dockables;
 
 import engine.Engine;
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -12,6 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -29,44 +36,99 @@ public class Image extends JPanel implements Dockable {
     private final JComboBox ImageSelector;
     private ImageIcon imageIcon;
     private Mat Image;
+    private Label x;
+    private Label y;
+    private JButton refresh;
+    private JPanel panel = new JPanel();
 
     public Image() {
-        
-        ImageSelector = new JComboBox(Engine.getInstance().getImagesNames());
+        add(panel, BorderLayout.WEST);
+        ImageSelector = new JComboBox();
+        String imagesNames[] = Engine.getInstance().getImagesNames();
+        ImageSelector.addItem("Choose");
+        for (int i = 0; i < imagesNames.length; i++) {
+            ImageSelector.addItem(imagesNames[i]);
+        }
+        ImageSelector.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ImageSelector.removeAllItems();
+                String imagesNames[] = Engine.getInstance().getImagesNames();
+                ImageSelector.addItem("Choose");
+                for (int i = 0; i < imagesNames.length; i++) {
+                    ImageSelector.addItem(imagesNames[i]);
+                }
+            }
+        });
         ImageSelector.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 try {
-                    removeAll();
-                    Image = Engine.getInstance().getImage(e.getItem().toString());
+                    panel.removeAll();
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        if (ImageSelector.getSelectedItem() != "Choose") {
+                            Image = Engine.getInstance().getImage(e.getItem().toString());
+                            imageIcon = new ImageIcon(Convert_To_Buffer(Image));
+                            panel.add(new JLabel(imageIcon));
+                        }
+                    }
+                    panel.revalidate();
+                    panel.repaint();
+                } catch (IOException ex) {
+                    Logger.getLogger(Image.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        x = new Label();
+        y = new Label();
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                x.setText("X = " + e.getX());
+                y.setText("Y = " + e.getY());
+            }
+        });
+        refresh = new JButton("refresh");
+        refresh.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    panel.removeAll();
+                        if (ImageSelector.getSelectedItem() != "Choose") {
+                    Image = Engine.getInstance().getImage(ImageSelector.getSelectedItem().toString());
                     imageIcon = new ImageIcon(Convert_To_Buffer(Image));
-                    add(new JLabel(imageIcon));
-                    revalidate();
+                    panel.add(new JLabel(imageIcon));
+                        }
+                    
+                    panel.revalidate();
+                    panel.repaint();
                 } catch (IOException ex) {
                     Logger.getLogger(Image.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
     }
+
     /**
      * Convert a mat to buffered image
      */
-    private BufferedImage Convert_To_Buffer(Mat image) throws IOException
-    {
+    private BufferedImage Convert_To_Buffer(Mat image) throws IOException {
         MatOfByte Byte_Mat = new MatOfByte();
         Highgui.imencode(".jpg", image, Byte_Mat);
-        byte[] Img = Byte_Mat.toArray();                      
+        byte[] Img = Byte_Mat.toArray();
         InputStream in = new ByteArrayInputStream(Img);
         BufferedImage Final_Image = ImageIO.read(in);
         return Final_Image;
- 
+
     }
+
     @Override
     public Component[] getNavigationComponents() {
 
         return new Component[]{
-            ImageSelector,};
-
+            ImageSelector,
+            refresh,
+            x,
+            y,};
     }
 
     @Override

2265a6c1808e0d00670650a90f969f47ba1ff1a1
elmohandharoon <elmohandharoon@yahoo.com>
2016-01-20 03:18:14 +0200

    excute button

--- src/ui/dockables/Command.java
+++ src/ui/dockables/Command.java
@@ -1,13 +1,16 @@
 package ui.dockables;
 
+import engine.Argument;
 import engine.Engine;
 import engine.ICommand;
 import engine.Parameter;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import ui.api.Dockable;
@@ -17,6 +20,7 @@ public class Command extends JPanel implements Dockable {
 
     private JComboBox commandSelector;
     private JPanel panel = new JPanel();
+    JButton excuteBotton = new JButton("execute");
 
     public Command() {
         add(panel, BorderLayout.WEST);
@@ -28,19 +32,32 @@ public class Command extends JPanel implements Dockable {
             public void itemStateChanged(ItemEvent e) {
                 panel.removeAll();
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-                    if (commandSelector.getSelectedItem()!= "Choose Command"){
-                    ICommand comm = Engine.getInstance()
-                            .getCommand(e.getItem().toString());
-                    Parameter[] params = comm.getParams();
-                    for (int i = 0; i < params.length; i++) {
-                        ArgumentEditor argumentEditor = ArgumentEditor.createInstance(params[i]);
-                        if (argumentEditor != null) {
-                            panel.add(argumentEditor.getEditorPanel());
+                    if (commandSelector.getSelectedItem() != "Choose Command") {
+                        ICommand comm = Engine.getInstance()
+                                .getCommand(e.getItem().toString());
+                        Parameter[] params = comm.getParams();
+                        Argument[] arrArguments = new Argument[2];
+                        for (int i = 0; i < params.length; i++) {
+                            ArgumentEditor argumentEditor = ArgumentEditor.createInstance(params[i]);
+                            arrArguments[i] = argumentEditor.getArgument();
+                            if (argumentEditor != null) {
+                                panel.add(argumentEditor.getEditorPanel());
+                            }
                         }
+                        excuteBotton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(java.awt.event.ActionEvent e) {
+                                comm.execute(arrArguments);
+
+                            }
+
+                        });
+
+                        panel.setLayout(new GridLayout(params.length, 2));
+                        panel.revalidate();
+                        panel.add(excuteBotton);
+                        panel.revalidate();
                     }
-                    panel.setLayout(new GridLayout(params.length, 2));
-                    panel.revalidate();
-                }
                 }
             }
         });

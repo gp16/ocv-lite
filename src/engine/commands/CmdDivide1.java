ba90eb4c1a3dfbd844f6fe7aa80488b1e24c1f42
elmohandharoon <elmohandharoon@yahoo.com>
2016-06-14 09:33:42 +0300

    Divide A Scalar By Image

---
+++ src/engine/commands/CmdDivide1.java
@@ -0,0 +1,52 @@
+package engine.commands;
+
+import engine.AbstractCommand;
+import engine.Engine;
+import engine.Parameter;
+import engine.Type;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+
+/**
+ *
+ * @author Elmohand Haroon
+ */
+public class CmdDivide1 extends AbstractCommand {
+
+    @Override
+    protected Parameter[] getParamsOnce() 
+    {
+        return new Parameter[]{
+        new Parameter("Source", Type.MAT_ID, 1, null, "image name to get from memory", false, false),    
+        new Parameter("Destination", Type.MAT_ID, 1, null, "image name to save into memory", false, false)};
+ 
+    }
+
+    @Override
+    protected Object executeSafe() 
+    {
+           String Source=getArgImgId("Source", 0);
+           String Destination=getArgImgId("Destination", 0);
+           Mat Source_Image = Engine.getInstance().getImage(Source);
+           Mat Destination_Image = new Mat();
+           
+           Core.divide(1000.0, Source_Image, Destination_Image);
+                   
+           Engine.getInstance().allocImage(Destination, Destination_Image);
+           return null; 
+    }
+
+    @Override
+    public String getName() 
+    {
+         return "Divide1";
+    }
+
+    @Override
+    public String getMan() 
+    {
+        return "Divide A Scalar By Image";
+    }
+    
+}

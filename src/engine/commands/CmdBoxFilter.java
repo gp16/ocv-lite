3c8a48cedff0e64df5ca65f3d2b9ac35c301d3d7
Abdelrahman Mohsen <abdelrahman.waziry@gmail.com>
2016-06-14 17:26:49 +0200

    rename boxfilter command

--- src/engine/commands/CmdBoxFilter.java
+++ src/engine/commands/CmdBoxFilter.java
@@ -4,7 +4,6 @@ import engine.AbstractCommand;
 import engine.Engine;
 import engine.Parameter;
 import engine.Type;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -14,21 +13,21 @@ public class CmdBoxFilter extends AbstractCommand {
     @Override
     protected Parameter[] getParamsOnce() {
         return new Parameter[]{
-            new Parameter("src", Type.MAT_ID, 1, null, "input image.", false, false),
-            new Parameter("dst",Type.MAT_ID, 1, null, "output image", false, false),
+            new Parameter("Source", Type.MAT_ID, 1, null, "input image.", false, false),
+            new Parameter("Destination",Type.MAT_ID, 1, null, "output image", false, false),
             new Parameter("ddepth",Type.INT, 1, Integer.MAX_VALUE, "the output"
                     + " image depth (-1 to use src.depth()).", false, false),
             new Parameter("size_x", Type.INT, 1, Integer.MAX_VALUE, "Kernel "
-                    + "size_x must be odd and bigger than 0", false,false),
+                    + "size_x must be bigger than 0", false,false),
             new Parameter("size_y", Type.INT, 1, Integer.MAX_VALUE, "Kernel "
-                    + "size_y must be odd and bigger than 0", false ,false)
+                    + "size_y must be bigger than 0", false ,false)
         };
     }
     
     @Override
     protected Object executeSafe() {
-        String Src = getArgImgId("src", 0);
-        String dest = getArgImgId("dst", 0);
+        String Src = getArgImgId("Source", 0);
+        String dest = getArgImgId("Destination", 0);
         int ddepth = getArgInt("ddepth",0);
         int size_x = getArgInt("size_x", 0);
         int size_y = getArgInt("size_y", 0);
@@ -46,12 +45,12 @@ public class CmdBoxFilter extends AbstractCommand {
     
     @Override
     public String getName() {
-        return "Boxfilter";
+        return "boxFilter";
     }
     
     @Override
     public String getMan() {
-        return "Transforms an image to compensate for lens distortion.";
+        return "Blurs an image using the box filter.";
     }
     
 }
